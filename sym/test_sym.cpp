fdbdb26abdd99a9208a156542e8f79c455058584
mau25rojas <mau.rojas25@gmail.com>
2022-02-27 11:54:13 +0100

    omg it works

--- sym/test_sym.cpp
+++ sym/test_sym.cpp
@@ -22,132 +22,125 @@ TEST(TestSymmetricDifference, BasicFunction) {
 
 // sym([1, 2, 3, 3], [5, 2, 1, 4]) should return [3, 4, 5].
 // sym([1, 2, 3, 3], [5, 2, 1, 4]) should contain only three elements.
-// TEST(TestSymmetricDifference1, BasicFunction) {
+TEST(TestSymmetricDifference1, BasicFunction) {
 
-//   Sym symTest;
+  Sym symTest;
 
-//   vector<int> array1{1, 2, 3, 3}; vector<int> array2{5, 2, 1, 4};
-//   set<int> expected{3,4,5};
+  vector<int> array1{1, 2, 3, 3}; vector<int> array2{5, 2, 1, 4};
+  set<int> expected{3,4,5};
 
-//   symTest.addInputArray(array1);
-//   symTest.addInputArray(array2);
-//   set<int> result = symTest.symDiff();
-//   symTest.print(result);
+  symTest.addInputArray(array1);
+  symTest.addInputArray(array2);
+  set<int> result = symTest.symDiff();
+  symTest.print(result);
 
-//   EXPECT_EQ(result.size(), 3);
-//   EXPECT_EQ(result, expected);
-// }
+  EXPECT_EQ(result.size(), 3);
+  EXPECT_EQ(result, expected);
+}
   
-// // sym([1, 2, 3], [5, 2, 1, 4, 5]) should return [3, 4, 5].
-// // sym([1, 2, 3], [5, 2, 1, 4, 5]) should contain only three elements.
-// TEST(TestSymmetricDifference2, BasicFunction) {
+// sym([1, 2, 3], [5, 2, 1, 4, 5]) should return [3, 4, 5].
+// sym([1, 2, 3], [5, 2, 1, 4, 5]) should contain only three elements.
+TEST(TestSymmetricDifference2, BasicFunction) {
 
-//   Sym symTest;
+  Sym symTest;
 
-//   vector<int> array1{1, 2, 3}; vector<int> array2{5, 2, 1, 4, 5};
-//   set<int> expected{3,4,5};
+  vector<int> array1{1, 2, 3}; vector<int> array2{5, 2, 1, 4, 5};
+  set<int> expected{3,4,5};
 
-//   symTest.addInputArray(array1);
-//   symTest.addInputArray(array2);
-//   set<int> result = symTest.symDiff();
-//   symTest.print(result);
+  symTest.addInputArray(array1);
+  symTest.addInputArray(array2);
+  set<int> result = symTest.symDiff();
+  symTest.print(result);
 
-//   EXPECT_EQ(result.size(), 3);
-//   EXPECT_EQ(result, expected);
-// } 
+  EXPECT_EQ(result.size(), 3);
+  EXPECT_EQ(result, expected);
+} 
 
-// // sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5]
-// // sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should contain only three elements.
-// TEST(TestSymmetricDifference3, BasicFunction) {
+// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5]
+// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should contain only three elements.
+TEST(TestSymmetricDifference3, BasicFunction) {
 
-//   Sym symTest;
+  Sym symTest;
 
-//   vector<int> array1{1, 2, 5}; vector<int> array2{2, 3, 5}; vector<int> array3{3, 4, 5};
-//   set<int> expected{1,4,5};
+  vector<int> array1{1, 2, 5}; vector<int> array2{2, 3, 5}; vector<int> array3{3, 4, 5};
+  set<int> expected{1,4,5};
 
-//   symTest.addInputArray(array1);
-//   symTest.addInputArray(array2);
-//   symTest.addInputArray(array3);
-//   set<int> result = symTest.symDiff();
-//   symTest.print(result);
+  symTest.addInputArray(array1);
+  symTest.addInputArray(array2);
+  symTest.addInputArray(array3);
+  set<int> result = symTest.symDiff();
+  symTest.print(result);
 
-//   EXPECT_EQ(result.size(), 3);
-//   EXPECT_EQ(result, expected);
-// } 
-
-
-// // sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
-// // sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain only three elements.
-// TEST(TestSymmetricDifference4, BasicFunction) {
-
-//   Sym symTest;
-
-//   vector<int> array1{1, 1, 2, 5}; vector<int> array2{2, 2, 3, 5}; vector<int> array3{3, 4, 5, 5};
-//   set<int> expected{1,4,5};
-
-//   symTest.addInputArray(array1);
-//   symTest.addInputArray(array2);
-//   symTest.addInputArray(array3);
-//   set<int> result = symTest.symDiff();
-//   symTest.print(result);
-
-//   EXPECT_EQ(result.size(), 3);
-//   EXPECT_EQ(result, expected);
-// }
+  EXPECT_EQ(result.size(), 3);
+  EXPECT_EQ(result, expected);
+} 
 
 
-// // sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7].
-// // sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
-// TEST(TestSymmetricDifference5, BasicFunction) {
+// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
+// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain only three elements.
+TEST(TestSymmetricDifference4, BasicFunction) {
 
-//   Sym symTest;
+  Sym symTest;
 
-//   vector<int> array1{3, 3, 3, 2, 5}; vector<int> array2{2, 1, 5, 7}; 
-//   vector<int> array3{3, 4, 6, 6}; vector<int> array4{1, 2, 3};
+  vector<int> array1{1, 1, 2, 5}; vector<int> array2{2, 2, 3, 5}; vector<int> array3{3, 4, 5, 5};
+  set<int> expected{1,4,5};
+
+  symTest.addInputArray(array1);
+  symTest.addInputArray(array2);
+  symTest.addInputArray(array3);
+  set<int> result = symTest.symDiff();
+  symTest.print(result);
+
+  EXPECT_EQ(result.size(), 3);
+  EXPECT_EQ(result, expected);
+}
+
+
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7].
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
+TEST(TestSymmetricDifference5, BasicFunction) {
+
+  Sym symTest;
+
+  vector<int> array1{3, 3, 3, 2, 5}; vector<int> array2{2, 1, 5, 7}; 
+  vector<int> array3{3, 4, 6, 6}; vector<int> array4{1, 2, 3};
   
-//   set<int> expected{2, 3, 4, 6, 7};
+  set<int> expected{2, 3, 4, 6, 7};
 
-//   symTest.addInputArray(array1);
-//   symTest.addInputArray(array2);
-//   symTest.addInputArray(array3);
-//   symTest.addInputArray(array4);
-//   set<int> result = symTest.symDiff();
-//   symTest.print(result);
+  symTest.addInputArray(array1);
+  symTest.addInputArray(array2);
+  symTest.addInputArray(array3);
+  symTest.addInputArray(array4);
+  set<int> result = symTest.symDiff();
+  symTest.print(result);
 
-//   EXPECT_EQ(result.size(), 3);
-//   EXPECT_EQ(result, expected);
-// } 
-
-
-// // sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
-// // sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements.
-// TEST(TestSymmetricDifference6, BasicFunction) {
-
-//   Sym symTest;
-
-//   vector<int> array1{3, 3, 3, 2, 5}; vector<int> array2{2, 1, 5, 7}; vector<int> array3{3, 4, 6, 6};
-//   vector<int> array4{1, 2, 3}; vector<int> array5{5, 3, 9, 8}; vector<int> array6{1};
-//   set<int> expected{1, 2, 4, 5, 6, 7, 8, 9};
-
-//   symTest.addInputArray(array1);
-//   symTest.addInputArray(array2);
-//   symTest.addInputArray(array3);
-//   symTest.addInputArray(array4);
-//   symTest.addInputArray(array5);
-//   symTest.addInputArray(array6);
-//   set<int> result = symTest.symDiff();
-//   symTest.print(result);
-
-//   EXPECT_EQ(result.size(), 3);
-//   EXPECT_EQ(result, expected);
-// } 
+  EXPECT_EQ(result.size(), 5);
+  EXPECT_EQ(result, expected);
+} 
 
 
-  
-  // // Expect two strings not to be equal.
-  // EXPECT_STRNE("hello", "world");
-  // // Expect equality.
-  // EXPECT_EQ(7 * 6, 42);
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements.
+TEST(TestSymmetricDifference6, BasicFunction) {
+
+  Sym symTest;
+
+  vector<int> array1{3, 3, 3, 2, 5}; vector<int> array2{2, 1, 5, 7}; vector<int> array3{3, 4, 6, 6};
+  vector<int> array4{1, 2, 3}; vector<int> array5{5, 3, 9, 8}; vector<int> array6{1};
+  set<int> expected{1, 2, 4, 5, 6, 7, 8, 9};
+
+  symTest.addInputArray(array1);
+  symTest.addInputArray(array2);
+  symTest.addInputArray(array3);
+  symTest.addInputArray(array4);
+  symTest.addInputArray(array5);
+  symTest.addInputArray(array6);
+  set<int> result = symTest.symDiff();
+  symTest.print(result);
+
+  EXPECT_EQ(result.size(), 8);
+  EXPECT_EQ(result, expected);
+}
 
 
 int main(int argc, char **argv) {
